2c6c17f8014d16e3af780be658d94ae426e5d074
professorik <36134599+professorik@users.noreply.github.com>
2022-01-18 20:23:50 +0200

    player

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,25 +1,29 @@
 extends KinematicBody2D
 
-export (int) var speed = 32
-var _speed = 32
-export (int) var jump_speed = -64
-export (int) var gravity = 40
+export (int) var speed = 180
+export (int) var jump_speed = -1000
+export (int) var gravity = 35
 
 var velocity = Vector2.ZERO
 
-func get_input():
-	velocity.x = 0
-	if Input.is_action_pressed("speed_up"):
-		speed = 64
+func _physics_process(_delta):
+	velocity.y += gravity
+	
 	if Input.is_action_pressed("ui_right"):
-		velocity.x += speed
+		velocity.x = speed
+		$ASprite.play("walk")
+		$ASprite.flip_h = false
 	if Input.is_action_pressed("ui_left"):
-		velocity.x -= speed
-
-func _physics_process(delta):
-	get_input()
-	velocity.y += gravity * delta
+		velocity.x = -speed
+		$ASprite.play("walk")
+		$ASprite.flip_h = true
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_speed
+		$ASprite.play("jump")
+	
+	if is_on_floor() and abs(velocity.x) < 0.1:
+		$ASprite.play("idle")
+	
 	velocity = move_and_slide(velocity, Vector2.UP)
-	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
-			velocity.y = jump_speed
+	
+	velocity.x = lerp(velocity.x, 0, 0.2)
