7a13c1df3d20119dab5499e2ebc73b63e567e18e
professorik <36134599+professorik@users.noreply.github.com>
2022-01-20 19:14:46 +0200

    hud

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -15,3 +15,15 @@ func _on_timer_timeout():
 	time += 1
 	$Time.text = String(time)
 	
+
+
+func _on_Resume_pressed():
+	$MiniMenu.set_visible(false)
+
+
+func _on_Settings_pressed():
+	pass # Replace with function body.
+
+
+func _on_Exit_pressed():
+	pass # Replace with function body.
