08a55c0d97d67a7e203ed34a767dabf2c60c6dd5
professorik <36134599+professorik@users.noreply.github.com>
2022-01-20 14:43:26 +0200

    fixed transformation

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -21,8 +21,9 @@ func _input(event):
 			var cell_sp = tilemap.get_cell_autotile_coord(tile_pos.x, tile_pos.y)
 			var cell2 = tilemap.get_cell(31 - tile_pos.x, tile_pos.y)
 			if cell != -1 and cell2 == -1:
+				var fl = tilemap.is_cell_x_flipped(tile_pos.x, tile_pos.y)
 				tilemap.set_cell(31 - tile_pos.x, tile_pos.y, cell, 
-				tile_pos.x < 16, false, false, cell_sp)
+				!fl, false, false, cell_sp)
 				tilemap.set_cell(tile_pos.x, tile_pos.y, -1)
 
 func _physics_process(_delta):
